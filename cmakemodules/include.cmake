3090faff0a2d4652867c5850ff6477e5f6c9b338
Daniel Shiposha <mrshiposha@gmail.com>
2018-01-08 08:37:17 +0300

    :sparkle: Setup CMakeLists.txt and cmakemodules

---
+++ cmakemodules/include.cmake
@@ -0,0 +1,2 @@
+INCLUDE_DIRECTORIES(include)
+INCLUDE_DIRECTORIES(include/3rd-party/osg/include)
---
+++ cmakemodules/libraries.cmake
@@ -0,0 +1,12 @@
+LINK_DIRECTORIES(lib/osg)
+
+SET(LIBRARIES
+	OpenThreads.lib
+	osg.lib
+	osgText.lib
+	osgUI.lib
+	osgWidget.lib
+	osgUtil.lib
+	osgDB.lib
+	osgViewer.lib
+)
